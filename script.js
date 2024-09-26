2888d963577637c66223296b7ee5db4a57d60580
minst13 <minamthapa38@yahoo.co.uk>
2024-09-26 15:11:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,9 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Select all tab links
-//     const tabLinks = document.querySelectorAll('.tab-links');
-
-//     // Add click event listener to each tab link
-//     tabLinks.forEach(function (tab) {
-//         tab.addEventListener('click', function () {
-//             // Remove active class from all tab links
-//             tabLinks.forEach(function (link) {
-//                 link.classList.remove('active');
-//             });
-
-//             // Add active class to the clicked tab link
-//             tab.classList.add('active');
-
-//             // Hide all tab contents
-//             hideAllTabContents();
-
-//             // Show the corresponding tab content based on the clicked tab
-//             if (tab.classList.contains('active-skills')) {
-//                 document.getElementById('skills').style.display = 'block';
-//             } else if (tab.classList.contains('active-experience')) {
-//                 document.getElementById('experience').style.display = 'block';
-//             } else if (tab.classList.contains('active-education')) {
-//                 document.getElementById('Education').style.display = 'block';
-//             }
-//         });
-//     });
-
-//     // Function to hide all tab contents
-//     function hideAllTabContents() {
-//         const tabContents = document.querySelectorAll('.tabContents');
-//         tabContents.forEach(function (content) {
-//             content.style.display = 'none';
-//         });
-//     }
-
-//     // Initially hide all content except the first one
-//     hideAllTabContents();
-//     document.getElementById('skills').style.display = 'block'; // Show Skills by default
-// });
-
-
-
 const tablinks = document.querySelectorAll(".tab-links");
 const tabContents = document.querySelectorAll(".tabContents");
 
 // Adding event listeners to all tab links
 tablinks.forEach(function (tablink, index) {
-    tablink.addEventListener('click', function (e) {
+    tablink.addEventListener("click", function (e) {
         // Call a function to handle the tab activation
         opentab(e, index);
     });
@@ -72,3 +28,19 @@ function opentab(e, index) {
     tabContents[index].classList.add("active-Content");
 }
 
+const closeBtn = document.querySelector(".fa-solid.fa-xmark");
+const openBtn = document.querySelector(".fa-solid.fa-bars");
+const sideBar = document.querySelector("#sideBar");
+
+function openMenu() {
+    sideBar.style.right = "0";
+    openBtn.style.display = "none";
+}
+
+function closeMenu() {
+    sideBar.style.right = "-200px";
+    openBtn.style.display = "block";
+}
+
+closeBtn.addEventListener("click", closeMenu)
+openBtn.addEventListener("click", openMenu)
