836bce8ce3bade18c60b25649c1704d985c80e67
minst13 <minamthapa38@yahoo.co.uk>
2024-10-02 23:54:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
+
 const tablinks = document.querySelectorAll(".tab-links");
 const tabContents = document.querySelectorAll(".tabContents");
 
-// Adding event listeners to all tab links
+
 tablinks.forEach(function (tablink, index) {
     tablink.addEventListener("click", function (e) {
-        // Call a function to handle the tab activation
         opentab(e, index);
     });
 });
 
-// Function to open the clicked tab and show the corresponding content
 function opentab(e, index) {
-    // Remove 'active-tab' from all tab links
     tablinks.forEach(function (tablink) {
         tablink.classList.remove("active-tab");
     });
 
-    // Remove 'active-Content' from all tab contents
+
     tabContents.forEach(function (tabContent) {
         tabContent.classList.remove("active-Content");
     });
 
-    // Add 'active-tab' to the clicked tab link (using e.currentTarget)
     e.currentTarget.classList.add("active-tab");
 
-    // Add 'active-Content' to the corresponding tab content
     tabContents[index].classList.add("active-Content");
 }
 
